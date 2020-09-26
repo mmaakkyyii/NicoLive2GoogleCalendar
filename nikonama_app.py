b4f5bba1a22dd0af809c13b7410046570105dab2
mmaakkyyii <kyoro3141592@gmail.com>
2020-09-27 04:00:26 +0900

    いらないところ削除

--- nikonama_app.py
+++ nikonama_app.py
@@ -300,17 +300,19 @@ class LineLive(Platform):
 		url = 'https://live-api.line-apps.com/web/v4.0/channel/'+channel_info;
 		res = requests.get(url).json()
 
-		if DEBUG==True:
-			print(res['upcomings'])
 		try:
+			if DEBUG==True:
+				print(res['upcomings'])
 			for r in res['upcomings']['rows']:
 				start_time=datetime.fromtimestamp(r['startAt'],timezone(timedelta(hours=9)))
 				finish_time=datetime.fromtimestamp(r['finishAt'],timezone(timedelta(hours=9)))
 				program_info_list.append(Program_info(r['id'],r['title'],start_time,finish_time))
 
 				if DEBUG==True:
+					print('id:',end='')
+					print(r['id']);
 					print('title:',end='')
-					print(title);
+					print(r['title']);
 					print('startAt:',end='')
 					print(start_time)
 					print('finishAt:',end='')
@@ -318,6 +320,7 @@ class LineLive(Platform):
 
 			return program_info_list
 		except:
+			print("in LineLive get_from_api")
 			return None
 
 	def get_channel_list(self,list_path):
@@ -331,7 +334,6 @@ class LineLive(Platform):
 		return linelive_channel_list
 
 
-
 class YoutubeLive(Platform):
 	def add_event(self,live_id,title,start_time,end_time):
 		self.add_to_calendar(title,live_id,contentId,start_time,end_time)
@@ -340,96 +342,6 @@ class YoutubeLive(Platform):
 	def add_event(self,live_id,title,start_time,end_time):
 		self.add_to_calendar(title,live_id,contentId,start_time,end_time)
 
-
-
-
-def add_linelive_event(title,contentId,start_time,end_time,calendar_url):
-	add_event(title,contentId,contentId,start_time,end_time,calendar_url)
-
-def add_nicolive_event(title,contentId,start_time,end_time,calendar_url):
-	add_event(title,contentId,'https://live2.nicovideo.jp/watch/'+contentId,start_time,end_time,calendar_url)
-
-
-def get_nicolive_from_title(title):#title of live
-	nicolive_API_endpoint="https://api.search.nicovideo.jp/api/v2/live/contents/search"
-	q=title
-	targets='title'
-
-	fields='contentId,channelId,title,startTime,liveEndTime'
-	filters_channelId=''
-	filters_liveStatus='&filters[liveStatus][0]=reserved' #enum('past','onair','reserved')
-
-	_sort='-startTime'
-	_context='nico live to google calendar'
-	_limit=str(10)
-
-	url=nicolive_API_endpoint+'?q='+q+'&targets='+targets+'&fields='+fields+filters_channelId+filters_liveStatus+'&_sort='+_sort+'&_context='+_context+'&_limit='+_limit
-
-	#print(url)
-	r = requests.get(url)
-	#print(r.json())
-	program_info_list=Program_info([],[],[],[])
-	for r_json in r.json():
-		program_info_list.append(Program_info(r_json['contentId'],r_json['title'],r_json['startTime'],r_json['liveEndTime']))
-	return program_info_list
-
-def get_LINE_LIVE(ch_id):#data[live_id,title,start,end]
-	url = 'https://live-api.line-apps.com/web/v4.0/channel/'+ch_id;
-	r = requests.get(url).json()
-	if DEBUG==True:
-		print(r['upcomings'])
-	try:
-		prog=r['upcomings']['rows'][0]
-
-		live_id=prog['id']
-		title=prog['title']
-		start_time=datetime.fromtimestamp(prog['startAt'],timezone(timedelta(hours=9)))
-		finish_time=datetime.fromtimestamp(prog['finishAt'],timezone(timedelta(hours=9)))
-		data={'live_id':live_id,'title':title,'start_time':start_time,'finish_time':finish_time}
-
-		if DEBUG==True:
-			print('title:',end='')
-			print(title);
-			print('startAt:',end='')
-			print(start_time)
-			print('finishAt:',end='')
-			print(finish_time)
-
-		return data
-	except:
-		return None
-
-
-
-def init():
-	nicolive_filename='channel_list/nicolive_channel_list.csv'
-	nicolive_channel_list=[]
-	with open(nicolive_filename, newline='',encoding="utf-8_sig") as csvfile:
-		spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-		for row in spamreader:
-			nicolive_channel_list.append(row)
-			if DEBUG==True:
-				print(', '.join(row))
-
-	linelive_filename='channel_list/linelive_channel_list.csv'
-	linelive_channel_list=[]
-	with open(linelive_filename, newline='',encoding="utf-8_sig") as csvfile:
-		spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-		for row in spamreader:
-			linelive_channel_list.append(row[1])
-			if DEBUG==True:
-				print(', '.join(row))
-	#print(nicolive_channel_list)
-	#print(linelive_channel_list)
-
-	MY_CALENDAR=''
-	with open('url/MY_CALENDAR_URL.txt', 'r') as cal:
-		MY_CALENDAR=str(cal.read())
-		if DEBUG==True:
-			print(MY_CALENDAR)
-
-	setting=[MY_CALENDAR,nicolive_channel_list,linelive_channel_list]
-	return setting
 
 def get_my_calendar():
 	with open('url/MY_CALENDAR_URL.txt', 'r') as cal:
@@ -438,7 +350,7 @@ def get_my_calendar():
 			print(MY_CALENDAR)
 		return MY_CALENDAR
 
-def main2():
+def main():
 	MY_CALENDAR=get_my_calendar()
 
 	nicolive = NicoLive(MY_CALENDAR,'channel_list/nicolive_channel_list.csv')
@@ -448,80 +360,9 @@ def main2():
 	linelive.update()
 
 
-def main():
-	setting=init()
-	MY_CALENDAR=setting[0]
-	nicolive_channel_list=setting[1]
-	linelive_channel_list=setting[2]
-
-	scheduled_contentID=get_calendar(MY_CALENDAR)
-	if DEBUG==True:
-		print(scheduled_contentID)
-
-	for ls in nicolive_channel_list:
-		if DEBUG==True:
-			print(ls)
-		schedule=get_nicolive(ls)
-		try:
-			for s in schedule['data']:
-				start_time=datetime.strptime(s['startTime'], '%Y-%m-%dT%H:%M:%S%z')
-				end_time=datetime.strptime(s['liveEndTime'], '%Y-%m-%dT%H:%M:%S%z')
-				if (s['contentId'] in scheduled_contentID):
-					print(' -',s['contentId'],s['title'],start_time,end_time)
-				else:
-					print(' @',s['contentId'],s['title'],start_time,end_time)
-					add_nicolive_event(s['title'],s['contentId'],start_time,end_time,MY_CALENDAR)
-		except KeyError:
-			print(schedule['meta'])
-			print("KeyError")
-
-	for ls in linelive_channel_list:
-		if DEBUG==True:
-			print(ls)
-		schedule=get_LINE_LIVE(ls)
-		if schedule==None:
-			continue
-		try:
-			start_time=schedule['start_time']
-			end_time=schedule['finish_time']
-			if (str(schedule['live_id']) in scheduled_contentID):
-				print(' -',schedule['live_id'],schedule['title'],start_time,end_time)
-			else:
-				print(' @',schedule['live_id'],schedule['title'],start_time,end_time)
-				add_linelive_event(schedule['title'],schedule['live_id'],start_time,end_time,MY_CALENDAR)
-		except KeyError:
-			print("KeyError")
-
-
-def add_non_registered_schedule(title):
-	setting=init()
-	nicolive_channel_list=setting[0]
-	MY_CALENDAR=setting[1]
-
-	scheduled_contentID=get_calendar(MY_CALENDAR)
-	if DEBUG==True:
-		print(scheduled_contentID)
-
-	schedule=get_nicolive_from_title(title)
-	if DEBUG==True:
-		print(schedule)
-	try:
-		data=schedule['data'][0];
-		if (data['contentId'] in scheduled_contentID):
-			print(' -',data['contentId'],data['title'],data['startTime'],data['liveEndTime'])
-		else:
-			print(' @',data['contentId'],data['title'],data['startTime'],data['liveEndTime'])
-			add_nicolive_event(data['title'],data['contentId'],data['startTime'],data['liveEndTime'],MY_CALENDAR)
-	except KeyError:
-		print(schedule['meta'])
-		print("KeyError")
-
-
-
 def usage():
 	print('.py            | nomal')
 	print('.py -d         | debug on')
-	print('.py -l         | line line')
 	print('.py -u \'url\' | add schedule of the url')
 	print('.py -d \'url\' | add schedule of the url with debug')
 
@@ -535,9 +376,6 @@ if __name__ =='__main__':
 		if option == '-d':
 			DEBUG=True
 			main()
-		elif option == '-2':
-			DEBUG=False
-			main2()
 		else:
 			usage()
 	elif len(args)==3:
